b597037d5f2b17546a546c4c053e042088af3fa9
vinodkumar.gaggara <vinodkumar.gaggara@asg.com>
2019-07-14 03:17:26 +0530

    Updates

---
+++ precache-manifest.12ab0f566467820059ced37f2b524042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44b5b0eb0f72c3b5b600145a3f2cd3c",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "4e4d682b69066bde6d09",
+    "url": "/weatherapp/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4e9f08eb4207d71b4b7b",
+    "url": "/weatherapp/static/css/main.a413c027.chunk.css"
+  },
+  {
+    "revision": "4e4d682b69066bde6d09",
+    "url": "/weatherapp/static/js/2.61b6dae8.chunk.js"
+  },
+  {
+    "revision": "4e9f08eb4207d71b4b7b",
+    "url": "/weatherapp/static/js/main.5d67b0d3.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  },
+  {
+    "revision": "70e2f0d62b23b105ea45650e53fa93fb",
+    "url": "/weatherapp/static/media/sunny.70e2f0d6.jpeg"
+  }
+]);
